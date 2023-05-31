3fb692c2cbe0e903885c983d88508c661bb52ac4
Gclean <ginogallo74@yahoo.com>
2023-05-31 15:07:33 -0400

    App configuration

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../styles/main.css";
+
+class App extends Component {
+  render() {
+    return <div className="page-wrapper"></div>;
+  }
+}
+
+export default App;
